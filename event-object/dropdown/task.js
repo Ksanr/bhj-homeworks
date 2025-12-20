9320db52894d2c53aa63812a6fe0137930f20caa
Aleksandr Lipunov <alipunov@mail.ru>
2025-12-20 15:09:02 +0000

    dropdown finish

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,17 @@
+const dropdown = document.querySelector(".dropdown__list");
+const dropdownValue = document.querySelector(".dropdown__value");
+
+//function toggleDropdown() {
+//  dropdown.classList.toggle('dropdown__list_active');
+//}
+
+function selectLink(event) {
+  if (event.target.classList.contains('dropdown__link')) {
+    event.preventDefault();
+    dropdownValue.textContent = event.target.textContent;
+  }
+  dropdown.classList.toggle('dropdown__list_active');
+}
+
+dropdownValue.addEventListener('click', selectLink);
+dropdown.addEventListener('click', selectLink);
